b4fd96e6acb435f39a430c4b82524dfee1cc5653
zhaoguodong <zhaoguodong@bitnei.cn>
2018-04-26 13:14:28 +0800

    增加数字格式化

--- src/main/java/com/bitnei/tools/common/NumberTool.java
+++ src/main/java/com/bitnei/tools/common/NumberTool.java
@@ -8,46 +8,57 @@ import java.text.NumberFormat;
 
 public class NumberTool {
 
-	private static final BigDecimal NUM_100 = new BigDecimal("100");
+    private static final BigDecimal NUM_100 = new BigDecimal("100");
 
-	public static BigDecimal toBigDecimal(String number) {
-		if (StringUtils.isEmpty(number)) {
-			return null;
-		}
-		return new BigDecimal(number);
-	}
+    public static BigDecimal toBigDecimal(String number) {
+        if (StringUtils.isEmpty(number)) {
+            return null;
+        }
+        return new BigDecimal(number);
+    }
 
-	public static String toMoneyStr(BigDecimal number) {
-		if (number == null) {
-			return "0.00";
-		}
-		NumberFormat format = new DecimalFormat("0.00");
-		return format.format(number);
-	}
+    public static String toMoneyStr(BigDecimal number) {
+        if (number == null) {
+            return "0.00";
+        }
+        NumberFormat format = new DecimalFormat("0.00");
+        return format.format(number);
+    }
 
-	public static String toPercent(BigDecimal number) {
-		if (number == null) {
-			return "0.00";
-		}
-		NumberFormat format = new DecimalFormat("0.#");
-		return format.format(number.multiply(NUM_100)) + "%";
-	}
+    public static String toPercent(BigDecimal number) {
+        if (number == null) {
+            return "0.00";
+        }
+        NumberFormat format = new DecimalFormat("0.#");
+        return format.format(number.multiply(NUM_100)) + "%";
+    }
 
-	public static Integer parseInt(String string) {
-		if (StringUtils.isEmpty(string)) {
-			return null;
-		}
-		try {
-			return Integer.valueOf(string);
-		} catch (Exception e) {
-			return null;
-		}
-	}
+    public static Integer parseInt(String string) {
+        if (StringUtils.isEmpty(string)) {
+            return null;
+        }
+        try {
+            return Integer.valueOf(string);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
-	public static String toString(Number number) {
-		if (number == null) {
-			return "";
-		}
-		return String.valueOf(number);
-	}
+    public static String toString(Number number) {
+        if (number == null) {
+            return "";
+        }
+        return String.valueOf(number);
+    }
+
+    public static String toPlainString(long number) {
+        return new BigDecimal(number).toPlainString();
+    }
+
+    public static String toPlainString(double number) {
+        if (Double.isInfinite(number) || Double.isNaN(number)) {
+            return null;
+        }
+        return new BigDecimal(number).setScale(6, BigDecimal.ROUND_HALF_UP).toPlainString();
+    }
 }
